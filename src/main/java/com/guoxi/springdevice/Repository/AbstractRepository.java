02a55c285c2d5b57ef1f22c9e1584782f26a99cc
guoxi <guoxi_789@126.com>
2022-09-17 17:17:16 +0800

    添加注解

--- src/main/java/com/guoxi/springdevice/Repository/AbstractRepository.java
+++ src/main/java/com/guoxi/springdevice/Repository/AbstractRepository.java
@@ -2,11 +2,12 @@ package com.guoxi.springdevice.Repository;
 
 import com.guoxi.springdevice.common.AbstractEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author guoxi@tg-hd.com
  * @since 2022/6/9$
  */
-
+@Repository
 public interface AbstractRepository<T extends AbstractEntity> extends PagingAndSortingRepository<T,String> {
 }
